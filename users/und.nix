cf060d5a9e5ae705d3084859f9c53e8aef0ed0eb
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2021-10-23 10:29:36 +0200

    feat (users): Remove some packages.
    With the arrival of new machines without an graphical interface I need
    to keep graphical applications in the user space. Furthermore the user
    configuration was a bit bloated for something that should be generic. I
    therefore moved some other package to their user counterpart.

--- users/und.nix
+++ users/und.nix
@@ -9,24 +9,8 @@
   extraGroups = [ "wheel" "networkmanager" "audio" "libvirtd" "docker"];
 
   packages = with pkgs; [
-    # Utils
-    zip unzip lz4 unrar
-    alacritty htop binutils
+    htop binutils
     gnupg
-
-    # Dev
-    rustup cargo
-    gcc m4 gnumake
-    clang universal-ctags radare2 valgrind clang-tools
-    cmake patchelf
-
-    # Virt
-    docker-compose virt-viewer virt-manager win-virtio
-
-    # Misc
-    firefox pavucontrol ncmpcpp imagemagick
-    neofetch feh
-    # Fonts
-    dejavu_fonts
+    neofetch
   ];
 }
